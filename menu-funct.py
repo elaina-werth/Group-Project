ed8a0125af2882ef3bd5e508a7c6b5747e6d1e1c
NotAGirlGamer <142840263+NotAGirlGamer@users.noreply.github.com>
2023-11-02 14:51:22 -0500

    Create menu-funct.py

---
+++ menu-funct.py
@@ -0,0 +1,10 @@
+def menu():
+    prompt = input('Do you want to chose the numbers? (y or n) ')
+    while prompt = 'y' or 'n':
+        if prompt = 
+    num1 = 1
+    num1 = int(input('Choose the minimum number'))
+    while num1 > 0
+    num2 = 1000
+    num2 = int(input('Choose the maximum number'))
+    
