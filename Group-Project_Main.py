0094823de224c57fa398d3250e1aa738b11e042a
elaina-werth <werthemw@gmail.com>
2023-11-03 14:48:42 -0500

    menu-funct

--- Group-Project_Main.py
+++ Group-Project_Main.py
@@ -3,4 +3,11 @@
 def main():
     #main accepts no arguments
     #it calls all functions necessary to create a number-guessing game
-    pass
+    
+    #define constants for menu options & default range
+    MIN = 1
+    MAX = 1000
+    
+    
+    
+    
--- menu-funct.py
+++ menu-funct.py
@@ -1,7 +1,14 @@
 def menu():
-    print('You can')
+    #menu accepts no arguments
+    #it displays a menu to the user, prompts for a selection
+    #menu returns a value for choice
+    
+    #display menu
+    print('You can:')
     print('Create a new game,(1)')
-    print('Choose your numbers,(2)')
-    print('and Exit(3)')
+    print('Choose a new range,(2)')
+    print('Or Exit(3)\n')
+    
+    #collect input from user
     choice = int(input('What would you like to do? '))
     return choice
