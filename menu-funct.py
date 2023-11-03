f99d6715d09bf29cf86eafa80720f07c4c633b9d
NotAGirlGamer <142840263+NotAGirlGamer@users.noreply.github.com>
2023-11-03 14:15:34 -0500

    needs the abilty to validate

--- menu-funct.py
+++ menu-funct.py
@@ -1,10 +1,12 @@
 def menu():
+    import random
     prompt = input('Do you want to chose the numbers? (y or n) ')
-    while prompt = 'y' or 'n':
-        if prompt = 
-    num1 = 1
-    num1 = int(input('Choose the minimum number'))
-    while num1 > 0
-    num2 = 1000
-    num2 = int(input('Choose the maximum number'))
-    
+    if prompt == 'y':
+        num1 = int(input('Choose the minimum number: '))
+        num2 = int(input('Choose the maximum number: '))
+        choice = random.randint(num1, num2)
+    if prompt == 'n':
+        num1 = 1
+        num2 = 1000
+        choice = random.randint(num1, num2)
+    return choice
