316c0dd42c171f520f27ecbc81c5f67b16bc5478
elaina-werth <werthemw@gmail.com>
2023-11-06 14:13:14 -0600

    Name_gen

--- Group-Project_Main.py
+++ Group-Project_Main.py
@@ -4,16 +4,9 @@ def main():
     #main accepts no arguments
     #it calls all functions necessary to create a number-guessing game
     
-<<<<<<< Updated upstream
-    #define constants for menu options & default range
-    MIN = 1
-    MAX = 1000
-    
-    
-    
-    
-=======
     #define constants for menu options and default range
     MIN = 1
     MAX = 1000
->>>>>>> Stashed changes
+    
+    #call menu
+    menu()
