cc1fadaa8725bc16c494595c47bca870eb620d5f
NotAGirlGamer <142840263+NotAGirlGamer@users.noreply.github.com>
2023-11-06 14:15:57 -0600

    Update Name_gen.py

--- Name_gen.py
+++ Name_gen.py
@@ -1,4 +1,6 @@
 def get_name():
+    #gets the names
     player1 = input("What is player one's name? ")
     player2 = input("what is player two's name? ")
-    return (player2, player1)
+    #return the names in order
+    return (player1, player2)
