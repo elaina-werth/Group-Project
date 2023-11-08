8df1ac0e88e6e6dc611d79582f6d979ef0c4ddc0
elaina-werth <werthemw@gmail.com>
2023-11-08 14:42:38 -0600

    Update game.py

--- game.py
+++ game.py
@@ -1,7 +1,36 @@
 #create the game
 
-def game(num, MIN, MAX):
+def game(random_number, MIN, MAX, player1, player2):
     #accepts an integer for number to be guessed, two inteers for min and max to validate range
     #players take turns guessing the number and receive feedback
     #tracks number of turns and validates out-of-range guesses
     print ("Welcome to the game!")
+    
+    keepgoing = 1
+    
+    while keepgoing == 1:
+        #player 1 turn
+        print(player1, ": Guess a number between", MIN, "and", MAX, end = '')
+        player_1_choice = int(input(": "))
+        
+        if player_1_choice < random_number:
+            print("A little higher!")
+        elif player_1_choice > random_number:
+            print("A little lower!")
+        else:
+            print ("Congratulations, you have guessed the number!")
+            keepgoing = 0
+            break
+            
+        #player 2 turn
+        print (player2, ": Guess a number between", MIN, "and", MAX, end = '')
+        player_2_choice = int(input(": "))
+        
+        if player_2_choice < random_number:
+            print("A little higher!")
+        elif player_2_choice > random_number:
+            print("A little lower!")
+        else:
+            print ("Congratulations, you have guessed the number!")
+            keepgoing = 0
+        
